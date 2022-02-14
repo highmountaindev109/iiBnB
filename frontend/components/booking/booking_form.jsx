b1ef5a4585504d894f1cf1576b62ebecde23196d
bluesky0115 <gold.star.bee@gmail.com>
2022-02-14 01:34:06 -0500

    Styling for form, calender validations

--- frontend/components/booking/booking_form.jsx
+++ frontend/components/booking/booking_form.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { fetchBookings } from "../../actions/booking_actions";
+import { AiFillStar } from "react-icons/ai";
 
 class BookingForm extends React.Component{
     constructor(props){
@@ -13,6 +14,7 @@ class BookingForm extends React.Component{
             number_of_guests: '1'
         }
         this.handleSubmit = this.handleSubmit.bind(this)
+        this.checkOutDate = this.checkOutDate.bind(this)
     }
 
     handleSubmit(e) {
@@ -27,36 +29,58 @@ class BookingForm extends React.Component{
         return e => this.setState({[field]: e.currentTarget.value})
     }
 
+    checkOutDate() {
+    var ms = new Date().getTime() + 86400000;
+    var tomorrow = new Date(ms);
+    return tomorrow.toISOString().split('T')[0]
+    }
+
     render() {
-        debugger
+        // debugger
         return(
             <div className="booking-form-container">
-                ${this.props.listing.price}/night
-                <form className="booking-form" onSubmit={this.handleSubmit}>
-                    <div className="booking-form-inner-box">
-                        <div className="booking-form-date">
-                            <div className="check-in-date-form" id="cd1">
-                                <label className="CIDF">Check In Date</label>
-                                <input type="date" onChange={this.update("check_in_date")} />
+                <div className="booking-formbox">
+                    <div className="bookingformtop">
+                        <div className="bookingformtopleft">
+                            <div className="bookingformprice">
+                            ${this.props.listing.price} {" "}
                             </div>
-                            <div className="check-in-date-form" id="cd2">
-                                <label className="CIDF">Check Out Date</label>
-                                <input type="date" onChange={this.update("check_out_date")} />
+                            <div className="bookingformprice2">
+                            {" "}/ night
                             </div>
                         </div>
-                        <div className="booking-form-guest">
-                            <label id="booking-guests">Number of guests</label>
-                            <select onChange={this.update("number_of_guests")}>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                            </select>
+                        <div className="bookingformtopright">
+                            <div className="rstar"><AiFillStar /> </div> 4.91 • 120 reviews
                         </div>
                     </div>
-                    <button>Book me</button>
-                </form>
+                    <form className="booking-form" onSubmit={this.handleSubmit}>
+                        <div className="booking-form-inner-box">
+                            <div className="booking-form-date">
+                                <div className="check-in-date-form" id="cd1">
+                                    <label className="CIDF">CHECK-IN</label>
+                                    <input type="date" onChange={this.update("check_in_date")} min={new Date().toISOString().split('T')[0]}/>
+                                </div>
+                                <div className="check-in-date-form" id="cd2">
+                                    <label className="CIDF">CHECKOUT</label>
+                                    <input type="date" onChange={this.update("check_out_date")} min={this.checkOutDate()} />
+                                </div>
+                            </div>
+                            <div className="booking-form-guest">
+                                <label id="booking-guests">NUMBER OF GUESTS </label>
+                                <select onChange={this.update("number_of_guests")}>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                </select>
+                            </div>
+                        </div>
+                        <button className="flexible_button" id="booking-button">Book me</button>
+                        <div> You won't be charged yet </div>
+                        <div> {this.checkOutDate()} </div>
+                    </form>
+                </div>
 
             </div>
         )
